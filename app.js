38f5cb09ee0aa43df244ed0f60c9eb44907e0f42
Ivan Erlich <ivanerlich@gmail.com>
2019-09-25 10:17:55 -0300

    checkpoint

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
 const express = require( 'express' );
 const app = express();
+const nunjucks = require("nunjucks");
+
+app.set('view engine', 'html'); // hace que res.render funcione con archivos html
+app.engine('html', nunjucks.render); // cuando le den archivos html a res.render, va a usar nunjucks
+nunjucks.configure('views'); // apunta a nunjucks al directorio correcto para los templates
+
+nunjucks.configure('views', {noCache: true});
 
 app.use(function (req, res, next) {
-  console.log("no")
+  console.log("middleware 1")
   next();
 });
 
 app.use("/special", function (req, res, next) {
-  console.log("especial")
+  console.log("middlewareSpecial")
   next();
 
 })
 
 app.get("/", function(req, res) {
-  console.log("khb")
-  res.send('hello world');
-})
+
+  var locals = {
+    // title: 'An Example',
+    people: [
+      { name: 'Gandalf'},
+      { name: 'Frodo' },
+      { name: 'Hermione'}
+    ]
+  };
+  nunjucks.render('index.html', {title: "personajes", people: locals.people}, function (err, output) {
+    res.send(output);
+  });
+});
 
 app.get("/special", function(req, res) {
   console.log("khb")
   res.send('hellsdaso world');
-})
+
+});
 
 
 
